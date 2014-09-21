3211d1d67f5777d755bacebbbce604d272a88c7e
jtierno <jonathan.tierno@gmail.com>
2014-09-21 18:22:10 -0300

    misc

--- maestro/src/maestro.cpp
+++ maestro/src/maestro.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : maestro.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
