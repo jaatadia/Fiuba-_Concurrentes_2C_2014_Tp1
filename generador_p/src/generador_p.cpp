41462c97462fa31ae9383757ef029a401e977e9e
jonathan <jonathan.tierno@gmail.com>
2014-09-26 17:42:43 -0300

    Creado el proyecto del generador

---
+++ generador_p/src/generador_p.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : generador_p.cpp
+// Author      : Jonathan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
